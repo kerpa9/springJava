9486eccc29d964a653f69983d81e85cbd32f98d4
Kevin Reyes <kerpa9@gmail.com>
2024-11-03 14:50:52 -0500

    Create evaluate episode top five

--- src/main/java/com/principal/Principal.java
+++ src/main/java/com/principal/Principal.java
@@ -1,10 +1,13 @@
 package com.principal;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import com.kevenreyes.model.DataSeason;
+import com.kevenreyes.model.EspisodeData;
 import com.kevenreyes.model.SeriesData;
 import com.kevenreyes.services.ConsultAPI;
 import com.kevenreyes.services.DataConverter;
@@ -13,8 +16,8 @@ public class Principal {
 
     private Scanner write = new Scanner(System.in);
     private ConsultAPI consultAPI = new ConsultAPI();
-    private String URL_BASE = "https://www.omdbapi.com/?t=";
-    private String ApiKey = "&apikey=e2a21826&t";
+    private final String URL_BASE = "https://www.omdbapi.com/?t=";
+    private final String ApiKey = "&apikey=e2a21826&t";
     // private String nameSerie = write.nextLine().replace(" ", "+");
     // private String url = URL_BASE + nameSerie + ApiKey;
     // Converter data
@@ -23,12 +26,12 @@ public class Principal {
 
     public void seeMenu() {
 
-        System.out.println("Write to find your favorite series");
+        System.out.println("Please Write to find your favorite series");
         String nameSerie = write.nextLine().replace(" ", "+");
         String url = URL_BASE + nameSerie + ApiKey;
         var json = consultAPI.dataObtain(url);
         var data = dataConverter.obtainData(json, SeriesData.class);
-        System.out.println(data);
+        // System.out.println(data);
 
         List<DataSeason> dataSeasons = new ArrayList<>();
         for (int i = 1; i <= data.totalTemp(); i++) {
@@ -41,9 +44,22 @@ public class Principal {
 
         }
 
+        // dataSeasons.forEach(System.out::println);
         System.out.println("-------------------EPISODE LIST-----------------------");
-        dataSeasons.forEach(System.out::println);
+        // dataSeasons.forEach(t -> t.episodes().forEach(e ->
+        // System.out.println(e.title())));
 
+        // Datas information list episode
+
+        List<EspisodeData> espisodeData = dataSeasons.stream().flatMap(t -> t.episodes().stream())
+                .collect(Collectors.toList());
+
+        // Top five episodes
+
+        System.out.println("Top five episodes");
+
+        espisodeData.stream().filter(e->!e.evaluation().equalsIgnoreCase("N/A")).sorted(Comparator.comparing(EspisodeData::evaluation).reversed()).limit(5)
+                .forEach(System.out::println);
     }
 
 }
